8112e855e7e38759dc733dd0fe8d340e44b691bf
oztuzelosman <95544310+oztuzelosman@users.noreply.github.com>
2023-04-28 02:05:38 +0000

    install all dependencies

--- backend/index.js
+++ backend/index.js
@@ -1,1 +1,10 @@
-console.log("memedali");
+const express = require("express");
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("Home Page");
+});
+
+app.listen(5000, () => {
+  console.log("listening");
+});
